183a2320439f5b2e024e194771eaff66651df76d
Jamal West <jwest23@ufl.edu>
2017-10-29 21:20:16 -0400

    profile pages w authen. and pass

---
+++ modules/profilepages/client/config/profilepages.client.config.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('profilepages')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['Menus'];
+
+  function menuConfig(menuService) {
+    // Set top bar menu items
+    menuService.addMenuItem('topbar', {
+      title: 'Profilepages',
+      state: 'profilepages',
+      type: 'dropdown',
+      roles: ['*']
+    });
+
+    // Add the dropdown list item
+    menuService.addSubMenuItem('topbar', 'profilepages', {
+      title: 'List Profilepages',
+      state: 'profilepages.list'
+    });
+
+    // Add the dropdown create item
+    menuService.addSubMenuItem('topbar', 'profilepages', {
+      title: 'Create Profilepage',
+      state: 'profilepages.create',
+      roles: ['user']
+    });
+  }
+}());
