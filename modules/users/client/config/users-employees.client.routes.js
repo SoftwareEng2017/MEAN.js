6dc4c509f9559327be24e53d7e400cbd589156a7
jairoarivas <jairoalbertorivas@gmail.com>
2017-10-31 02:14:05 -0400

    Fixed small indentation bug

--- modules/users/client/config/users-employees.client.routes.js
+++ modules/users/client/config/users-employees.client.routes.js
@@ -5,15 +5,15 @@ angular.module('users.employees.routes').config(['$stateProvider',
   function ($stateProvider) {
     $stateProvider
     .state('employees', {
-        abstract: true,
-        url: '/employees',
-        template: '<ui-view/>'
-      })
+      abstract: true,
+      url: '/employees',
+      emplate: '<ui-view/>'
+    })
     .state('employees.users', {
       url: '/employees/users',
       templateUrl: 'modules/users/client/views/admin/list-users.client.view.html',
       controller: 'UserListController'
     });
-   
+
   }
 ]);
